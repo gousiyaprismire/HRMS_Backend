34875e32c3c412026a9621c91b028d50fa472a4e
manjunadhkonjeti3 <manjunadhkonjeti3@gmail.com>
2025-03-11 18:12:26 +0530

    Completed API's

--- src/main/resources/scripts/create-scripts/attendanceandtime/leave_applications.sql
+++ src/main/resources/scripts/create-scripts/attendanceandtime/leave_applications.sql
@@ -1,6 +1,7 @@
 CREATE TABLE leave_applications (
     id INT PRIMARY KEY AUTO_INCREMENT,
     emp_id VARCHAR(50) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     leave_type ENUM('Sick Leave', 'Vacation', 'Casual Leave', 'Maternity Leave', 'Paternity Leave', 'Other') NOT NULL,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,

fb7f5d334b138d634649a4c564694de4e211b820
tkpriya13 <keerthi.lanka13@gmail.com>
2025-03-13 08:13:55 +0530

    Modifications

--- src/main/java/com/hrms/controller/Recruitment/InterviewController.java
+++ src/main/java/com/hrms/controller/Recruitment/InterviewController.java
@@ -5,6 +5,9 @@ import com.hrms.service.Recruitment.InterviewService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;

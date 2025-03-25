5e9ffa2523c6170cdfa37686799c53a8300aa19d
gousiyaprismire <gousiyaprismire@gmail.com>
2025-03-25 11:28:52 +0530

    GOC

--- src/main/java/com/hrms/controller/PerformanceManagement/GeneralOptionController.java
+++ src/main/java/com/hrms/controller/PerformanceManagement/GeneralOptionController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.hrms.service.PerformanceManagement.GeneralOptionService;
 
 @RestController
 @RequestMapping("/api/general_options")
+@CrossOrigin(origins = "http://localhost:3000")
 public class GeneralOptionController {
 	
 	 @Autowired
